90716eda7f428efc2346abbd62e2cc36d967b5c3
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2020-04-29 16:29:02 +0530

    Added local reference of nuget packages

--- SampleAppAdobeAnalytics/SampleAppAdobeAnalytics/App.cs
+++ SampleAppAdobeAnalytics/SampleAppAdobeAnalytics/App.cs
@@ -4,15 +4,9 @@ namespace SampleAppAdobeAnalytics
 {
     public class App
     {
-        public static bool UseMockDataStore = true;
-        public static string BackendUrl = "http://localhost:5000";
-
         public static void Initialize()
         {
-            if (UseMockDataStore)
-                ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
-            else
-                ServiceLocator.Instance.Register<IDataStore<Item>, CloudDataStore>();
+            ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
         }
     }
 }
